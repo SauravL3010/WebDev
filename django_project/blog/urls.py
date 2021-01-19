ca782cb361b67382b84fef950d11628bfcf8f09a
Saurav Gurhale <slgurhal@uwaterloo.ca>
2021-01-19 16:52:05 -0500

    DataBAse added

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name = 'blog-home'),
-    path('about/', views.about, name = 'blog-view')
+    path('about/', views.about, name = 'blog-about')
 ]
